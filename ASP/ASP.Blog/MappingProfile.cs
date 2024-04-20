9d1984b27e7aff658c1f0c68a2311c05bf129236
valmon55 <fedor_ka@mail.ru>
2024-04-20 22:53:14 +0300

    Все статьи выводятся на экран

--- ASP/ASP.Blog/MappingProfile.cs
+++ ASP/ASP.Blog/MappingProfile.cs
@@ -25,8 +25,8 @@ namespace ASP.Blog.BLL
                 .ForMember(x => x.UserId, opt => opt.MapFrom(c => c.User.Id));
             CreateMap<Article, ArticleViewModel>()
                 .ForMember(x => x.Content, opt => opt.MapFrom(c => c.Content))
-                .ForMember(x => x.User, opt => opt.MapFrom(c => c.User))
-                .ForMember(x => x.User.Id, opt => opt.MapFrom(c => c.UserId));
+                .ForMember(x => x.User, opt => opt.MapFrom(c => c.User));
+                //.ForMember(x => x.User.Id, opt => opt.MapFrom(c => c.UserId));
             //CreateMap<AllArticlesViewModel, List<ArticleViewModel>>();
         }
     }
